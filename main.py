225d4e9a11725dddd0bf7af6f8badfcf22aae84e
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-01 22:17:20 +0100

    put the right enviroment in

---
+++ main.py
@@ -0,0 +1,8 @@
+import gymnasium as gym 
+'''
+you need to have done 'pip install gymnasium[MuJoCo]' in the terminal 
+if it doesnt work try making a virtual enviroment in this folder called .venv (ask me if you need help)
+'''
+
+env = gym.make('HumanoidStandup-v4')
+
