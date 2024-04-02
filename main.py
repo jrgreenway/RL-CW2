acc72571e0c56e971dad4b0f87bb3bf8ff6d808c
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-02 14:12:06 +0100

    added some instructions to installing the enviroment

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import gymnasium as gym 
 import numpy as np
+import mujoco
 from Agent import Agent
 '''
-you need to have done 'pip install gymnasium[MuJoCo]' in the terminal 
+I suggest you make a virtual enviroment in the code folder called .venv - you can do it on vscode
+by putting '>Python: Select Interpreter' then pressing 'create virtual enviroment' then venv.
+then in the terminal at the bottom (should say (.venv) before your path) do:
+'pip install gymnasium[MuJoCo]', then 'pip uninstall mujoco', then 'pip install mujoco==2.3.7'
+because the newest version doesnt work with gymnasium.
+
 if it doesnt work try making a virtual enviroment in this folder called .venv (ask me if you need help)
 the docs for the env -> https://gymnasium.farama.org/environments/mujoco/humanoid_standup/
 '''
 
-env = gym.make('HumanoidStandup-v4')
+env = gym.make('HumanoidStandup-v4',render_mode = 'human')
 # print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_array']
 total_episodes = 100
 agent = Agent(env)
@@ -15,7 +21,8 @@ for episode in range(total_episodes):
     observation, info = env.reset()
     terminated = False
     while not terminated:
-        action = agent.choose_action()
+        env.render()
+        action = np.full((17,), 0.2)#agent.choose_action()
         observation, reward, terminated, truncated, info = env.step(action)
         
 env.close()
