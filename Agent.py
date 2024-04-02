7161a3ddd43f89f4995cc779c6c543e3cab8d259
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-02 13:29:14 +0100

    couple more bits

---
+++ Agent.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+    
+    def choose_action(self) -> np.ndarray[17,]:
+        '''Needs to return an action array of shape (17,)'''
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import gymnasium as gym 
+import numpy as np
+from Agent import Agent
 '''
 you need to have done 'pip install gymnasium[MuJoCo]' in the terminal 
 if it doesnt work try making a virtual enviroment in this folder called .venv (ask me if you need help)
+the docs for the env -> https://gymnasium.farama.org/environments/mujoco/humanoid_standup/
 '''
 
 env = gym.make('HumanoidStandup-v4')
-
+# print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_array']
+total_episodes = 100
+agent = Agent(env)
+for episode in range(total_episodes):
+    observation, info = env.reset()
+    terminated = False
+    while not terminated:
+        action = agent.choose_action()
+        observation, reward, terminated, truncated, info = env.step(action)
+        
+env.close()
