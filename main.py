64fc93db2a237dd4ff20bc5fa6ab9931555e3bb7
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-10 17:11:45 +0100

    changed enviroment cos its bad

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import gymnasium as gym 
 import numpy as np
-import mujoco
 from Agent import Agent
-'''
-I suggest you make a virtual enviroment in the code folder called .venv - you can do it on vscode
-by putting '>Python: Select Interpreter' then pressing 'create virtual enviroment' then venv.
-then run 'pip install -r requirements.txt' in the terminal at the bottom of vscode
-because the newest version doesnt work with gymnasium.
 
-if it doesnt work try making a virtual enviroment in this folder called .venv (ask me if you need help)
-the docs for the env -> https://gymnasium.farama.org/environments/mujoco/humanoid_standup/
-'''
 
-env = gym.make('HumanoidStandup-v4',render_mode = 'human')
+env = gym.make('HumanoidStandup-v4',render_mode = 'human')#change
 # print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_array']
 total_episodes = 100
 agent = Agent(env)
