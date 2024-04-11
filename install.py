a0737c9c9cf35f3e298142ed37753e7077483323
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-11 16:04:13 +0100

    add check for existing venv in install.py

--- install.py
+++ install.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 import os
 
+
+        
 def create_venv(venv_name):
     subprocess.check_call([sys.executable, "-m", "venv", venv_name])
 
@@ -12,6 +14,8 @@ packages = ["gymnasium[atari]", "gymnasium[accept-rom-license]"]
 venv_name = ".venv"
 
 if __name__ == "__main__":
+    if os.path.exists(venv_name):
+        raise Exception(f"Error: {venv_name} already exists. Please delete before running this script")
     create_venv(venv_name)
     for package in packages:
         install(package, venv_name)
