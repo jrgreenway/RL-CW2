bdf73a9e258e1ab467f57603d5fbc7b55ab319ae
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-11 15:27:45 +0100

    random movements as test

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import numpy as np
 from Agent import Agent
 
 
-env = gym.make('HumanoidStandup-v4',render_mode = 'human')#change
-# print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_array']
+env = gym.make("ALE/Surround-v5", render_mode='human')
+action_space = env.action_space
+print(action_space)
+# print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_arr
 total_episodes = 100
 agent = Agent(env)
 for episode in range(total_episodes):
@@ -12,7 +14,7 @@ for episode in range(total_episodes):
     terminated = False
     while not terminated:
         env.render()
-        action = np.full((17,), 0.2)#agent.choose_action()
+        action = env.action_space.sample()#agent.choose_action()
         observation, reward, terminated, truncated, info = env.step(action)
         
 env.close()
