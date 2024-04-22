099bca7a4281a953cbb57b4cadb14d9c5b21e10e
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-22 16:32:35 +0100

    agent and updated main

--- Agent.py
+++ Agent.py
@@ -1,9 +1,68 @@
+#Imports
+#from DQN import DQN #Change According to NN naming
+#import torch
+#import torch.nn.functional as F
 import numpy as np
 
-class Agent:
-    def __init__(self, env):
-        self.env = env
+
+class Agent():
+    def __init__(self, memory_capacity=0, batch_size=0, observation_space=0, action_space=0, hidden_neurons=0, learning_rate=0, epsilon=0.3, gamma=0.95) -> None:
+        #Neural Network Training Device
+        #self.device = torch.device("cpu")
+        
+        #Experience Replay
+        #Put Replay Object here
+        
+        #Policy Network
+        #self.network = 
+        #self.target_network = 
+        #self.target_network.load_state_dict(self.network.state_dict())
+        #self.target_network.eval()
+        #self.network.eval()
+        
+        #Network Optimiser - Have a look at different optimisers
+        #The optimiser updates the weightings in the nn to predict better q-values.
+        #self.optimiser = torch.optim.Adam(self.network.parameters(), lr=learning_rate)
+        
+        #Hyperparameters
+        self.epsilon = epsilon
+        self.gamma = gamma
+        
+    def action(self, state):
+        # if torch.rand(1) < self.epsilon:
+        #     return torch.randint(0, self.network.output_size, (1,))
+        # else:
+        #     with torch.no_grad():
+        #         action = self.network(state).argmax()#Update according to nn parameters
+        action = np.random.randint(0,5)
+        return action
     
-    def choose_action(self) -> np.ndarray[17,]:
-        '''Needs to return an action array of shape (17,)'''
+    def learn_replay(self):
+        #Insert Experience replay learning here
         pass
+    
+    def update_target(self):
+        self.target_network.load_state_dict(self.network.state_dict())
+        
+    
+    def episode(self, env):
+        state, info = env.reset()
+        total_reward = 0
+        terminated = False
+        while not terminated:
+            action = self.action(state)
+            next_state, reward, terminated, truncated, info = env.step(action)
+            #Put Experience Replay Here
+            state = next_state
+            total_reward += reward
+        #self.update_target()
+        return total_reward
+    
+    def train(self, env, episodes):
+        #Add in performance indicators
+        rewards = []
+        for episode in range(episodes):
+            reward = self.episode(env)
+            rewards.append(reward)
+        return rewards
+    
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import gymnasium as gym 
 import numpy as np
 from Agent import Agent
+from matplotlib import pyplot as plt
 
 
-env = gym.make("ALE/Surround-v5", render_mode='human')
+env = gym.make("ALE/Surround-v5")#render_mode='human'
 action_space = env.action_space
 print(action_space)
 # print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_arr
 total_episodes = 100
-agent = Agent(env)
-for episode in range(total_episodes):
-    observation, info = env.reset()
-    terminated = False
-    while not terminated:
-        env.render()
-        action = env.action_space.sample()#agent.choose_action()
-        observation, reward, terminated, truncated, info = env.step(action)
+agent = Agent()
+rewards = agent.train(env, total_episodes)
+plt.plot(rewards)
+plt.show()
         
 env.close()
