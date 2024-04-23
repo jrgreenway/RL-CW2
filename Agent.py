32da5300b10e6c73a38e71aebdb28d11f36a5468
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-23 22:17:32 +0100

    couple updates

--- Agent.py
+++ Agent.py
@@ -1,36 +1,38 @@
 #Imports
 #from DQN import DQN #Change According to NN naming
-#import torch
+import torch
 #import torch.nn.functional as F
 import numpy as np
 
 
 class Agent():
-    def __init__(self, memory_capacity=0, batch_size=0, observation_space=0, action_space=0, hidden_neurons=0, learning_rate=0, epsilon=0.3, gamma=0.95) -> None:
+    def __init__(self, observation_size, action_size, epsilon=0.3, gamma=0.95, **kwargs):
         #Neural Network Training Device
         #self.device = torch.device("cpu")
+        
+        #Env Infomation
+        self.observation_size = observation_size
+        self.action_size = action_size
         
         #Experience Replay
         #Put Replay Object here
         
-        #Policy Network
+        #Policy Network - If we want a target network
         #self.network = 
         #self.target_network = 
-        #self.target_network.load_state_dict(self.network.state_dict())
-        #self.target_network.eval()
-        #self.network.eval()
         
-        #Network Optimiser - Have a look at different optimisers
-        #The optimiser updates the weightings in the nn to predict better q-values.
-        #self.optimiser = torch.optim.Adam(self.network.parameters(), lr=learning_rate)
+        
+        #Network Optimiser
+        #self.optimiser = 
         
         #Hyperparameters
         self.epsilon = epsilon
+        self.epsilon_decay = kwargs.get('epsilon_decay', 0.99)
         self.gamma = gamma
         
     def action(self, state):
         # if torch.rand(1) < self.epsilon:
-        #     return torch.randint(0, self.network.output_size, (1,))
+        #     return torch.randint(0, self.action_size, (1,))
         # else:
         #     with torch.no_grad():
         #         action = self.network(state).argmax() #Update according to nn parameters
@@ -43,19 +45,21 @@ class Agent():
     
     def update_target(self):
         self.target_network.load_state_dict(self.network.state_dict())
-        
+
+    def decay(self):
+        self.epsilon = max(self.epsilon_minimum ,self.epsilon * self.epsilon_decay)
     
     def episode(self, env):
         state, info = env.reset()
         total_reward = 0
         terminated = False
-        while not terminated:
+        truncated = False
+        while not terminated and not truncated:
             action = self.action(state)
             next_state, reward, terminated, truncated, info = env.step(action)
             #Put Experience Replay Here
             state = next_state
             total_reward += reward
-        #self.update_target()
         return total_reward
     
     def train(self, env, episodes):
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import gymnasium as gym 
 import numpy as np
 from Agent import Agent
-from matplotlib import pyplot as plt
-
 
 env = gym.make("ALE/Surround-v5")#render_mode='human'
-action_space = env.action_space
-print(action_space)
-# print(env.metadata) -> 'render_modes': ['human', 'rgb_array', 'depth_arr
 total_episodes = 10
-agent = Agent()
+agent = Agent(env.observation_space.shape, env.action_space.n)
 rewards = agent.train(env, total_episodes)
-plt.plot(rewards)
-plt.show()
         
 env.close()
