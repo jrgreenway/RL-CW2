cde490e5eefe2483fe070f3bd62210af7623082b
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-04-26 15:34:07 +0000

    docker works!

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import gymnasium as gym 
-import numpy as np
-from Agent import Agent
+from pettingzoo.atari import surround_v2
 
-env = gym.make("ALE/Surround-v5")#render_mode='human'
-total_episodes = 10
-agent = Agent(env.observation_space.shape, env.action_space.n)
-rewards = agent.train(env, total_episodes)
-        
+env = surround_v2.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        action = env.action_space(agent).sample() # this is where you would insert your policy
+
+    env.step(action)
 env.close()
