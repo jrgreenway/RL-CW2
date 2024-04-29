ff3f4b6d9d1e592205350669d4fb32e451647202
Maximillian Atherton <max@a319.co.uk>
2024-04-29 13:37:48 +0100

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -4,20 +4,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 class Helper():
     def __init__(self):
-        self.data = {'Episode': [],
-                     'Total Reward': [],
-                     'Epsilon': [],
-                     'Loss': []}
-        self.writer = SummaryWriter()
+        self.data = pd.DataFrame()
 
-    def add_data(self, episode, total_reward, epsilon, loss):
-        self.data['Episode'].append(episode)
-        self.data['Total Reward'].append(total_reward)
-        self.data['Epsilon'].append(epsilon)
-        self.data['Loss'].append(loss)
+    def add_data(self, data):
+        self.data = self.data.append(data)
 
     def log_to_tensorboard(self):
-        for i in range(len(self.data['Episode'])):
+        for i in range(len(self.data[0])):
             self.writer.add_scalar('Total Reward', self.data['Total Reward'][i], self.data['Episode'][i])
             self.writer.add_scalar('Epsilon', self.data['Epsilon'][i], self.data['Episode'][i])
             self.writer.add_scalar('Loss', self.data['Loss'][i], self.data['Episode'][i])
@@ -25,9 +18,4 @@ class Helper():
     def close_tensorboard(self):
         self.writer.close()
 
-# Logging data to TensorBoard
-helper.log_to_tensorboard()
-
-# Close TensorBoard writer
-helper.close_tensorboard()
     
