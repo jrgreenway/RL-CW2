fc4b016d0bd4b792fe53a8701a463f67189adc9c
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-05-02 13:24:14 +0100

    done

--- DeepQNetworkAgent.py
+++ DeepQNetworkAgent.py
@@ -1,5 +1,6 @@
 #Imports
 #from DQN import DQN #Change According to NN naming
+import gymnasium
 import torch as T
 import torch.nn as nn               # Neural Network (nn)
 # We may need to import something for convulutional layers because we are working with images
@@ -7,7 +8,9 @@ import torch.nn.functional as F     # Activation Functions
 import torch.optim as optim         # nn Optimiser
 import numpy as np                  # numpy
 from termcolor import colored       # Colored text for debugging
-import os                           # For file joining operations to handle model checkpointing
+import os
+
+from tqdm import tqdm                           # For file joining operations to handle model checkpointing
 
 # This inmplementation has no target network or convulutional layers
 # Convulutional layers are used to process the image and extract features from it
@@ -30,17 +33,17 @@ class ReplayMemory():
 
 
     
-    def store_transition(self, state, action, reward, next_state, terminated, truncated):
+    def store_transition(self, state, action, reward, next_state, done):
         #Store transitions in memory so that we can use them for experience replay   
         self.state_memory[self.memory_index] = state
         self.action_memory[self.memory_index] = action
         self.reward_memory[self.memory_index] = reward
-        self.new_state_memory[self.memory_index] = next_state
-        self.done_memory[self.memory_index] = terminated
-        self.memory_index = (self.memory_index + 1) % self.max_size
-        self.size = min(self.size + 1, self.max_size)
+        self.next_state_memory[self.memory_index] = next_state
+        self.done_memory[self.memory_index] = done
+        self.memory_index = (self.memory_index + 1) % self.memory_size
+        self.size = min(self.size + 1, self.memory_size)
     
-    def sample_batch(self):
+    def sample(self):
         #Returns a batch of random experiences of size batch_size
         batch_indexes = np.random.choice(self.size, size=self.batch_size, replace=False)
         return dict(states=self.state_memory[batch_indexes], 
@@ -58,44 +61,38 @@ class DuelingDeepQNetwork(nn.Module):
     # fc1_dims      = fully connected layer 1 dimensions
     # fc2_dims      = fully connected layer 2 dimensions
     # n_actions     = number of actions
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, checkpoint_dir, name):
-        super(DuelingDeepQNetwork, self).__init__()    # Calls nn.Module __init__ function to set up PyTorch stuff
-        self.lr = lr
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
+    def __init__(self, lr, input_dims, nn_dims, n_actions, checkpoint_dir, name):
+        super(DuelingDeepQNetwork, self).__init__()    # Inheriting from nn.Module
 
         self.checkpoint_dir = checkpoint_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name)  # Save our checkpoint directory for later use
 
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)   # *self.input_dims is the same as self.input_dims[0], self.input_dims[1], etc.
-        self.V = nn.Linear(fc1_dims, 1)             # Value stream: tells the agent theh value of thhe current state
-        self.A = nn.Linear(fc1_dims, n_actions)     # Advantage stream: tells the agent the relative advantage of eachh action in a given state
-        #self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)      # Create a fully connected layer with fc1_dims inputs and fc2_dims outputs
-        #self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.feature = nn.Sequential(   #Feature layer that leads into advantage and value streams
+            nn.Linear(input_dims, nn_dims),
+            nn.ReLU(),
+        )
+        
+        self.V = nn.Sequential( # Value stream: tells the agent the value of the current state
+            nn.Linear(nn_dims, nn_dims),
+            nn.ReLU(),
+            nn.Linear(nn_dims, 1),
+        )
+        
+        self.A = nn.Sequential( # Advantage stream: tells the agent the relative advantage of eachh action in a given state
+            nn.Linear(nn_dims, nn_dims),
+            nn.ReLU(),
+            nn.Linear(nn_dims, n_actions)
+        )     
 
-        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)  # Create an Adam optimizer to adjust weights of the nn with the learning rate lr
-        self.loss = nn.MSELoss()  # Mean Squared Error Loss function. 2 inputs: input and target. Returns the mean squared error between the input and target
-        #print(f"GPU available?" + colored(T.cuda.is_available(), 'green'))
-        #self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')  # Use GPU if your PC has one (better), if not use
-        self.device = T.device('cpu')  # Use CPU
-        self.to(self.device)    # Move the nn to the device (GPU or CPU)
-
-    # We don't need to handle backpropagation manually because PyTorch does it for us
-    # Forward function is called when you pass data through the nn
     def forward(self, state):
-        x = F.relu(self.fc1(state))     # Pass state into first layer and apply ReLU activation function in each neuron in the layer
-        # x = F.relu(self.fc2(x))         # Pass output from 1st layer into 2nd layer
-        # actions = self.fc3(x)           # Pass output from 2nd layer into 3rd layer but don't apply action function, just save it
+        x = self.feature(state)
         V = self.V(x)
         A = self.A(x)
-        
-        return V, A
+        return V + A - A.mean(dim=-1, keepdim=True)
     
     #Save and load checkpoints
     def save_checkpoint(self):
-        print('... saving checkpoint ...')
+        #print('... saving checkpoint ...')
         T.save(self.state_dict(), self.checkpoint_file)
 
     def load_checkpoint(self, file):
@@ -105,20 +102,19 @@ class DuelingDeepQNetwork(nn.Module):
 
 
 class DQNAgent():
-    # observation_Space = Number of inputs  (same as input_dims)
-    # action_space = Number of outputs (like n_actions)
-    # hidden_neurons = Number of neurons in the hidden layers (same as fc1_dims and fc2_dims)
-    def __init__(self, learning_rate, batch_size, observation_space, 
-                  n_actions, gamma, epsilon, max_memory_size=100000, hidden_neurons=64, eps_decay=5e-4, eps_min=0.01, replace=1000, checkpoint_dir='tmp/dueling_ddqn'): 
+    def __init__(self, env: gymnasium.Env, learning_rate, batch_size, gamma, epsilon, max_memory_size=100000, hidden_neurons=64, eps_decay=0.995, eps_min=0.1, replace=1000, checkpoint_dir='tmp/'): 
         # Adjust epsilon decay rate later, right now linear decay
 
+        self.env = env
         self.learning_rate = learning_rate
         self.batch_size = batch_size
-        self.observation_space = observation_space
-        self.n_actions = n_actions
-        self.action_space = [i for i in range(n_actions)] # Represents all possible actions. Makes it easier to select random actions
-        self.hidden_neurons = hidden_neurons
+        self.observation_space = env.observation_space
+        self.observation_shape = env.observation_space.shape
+        self.n_actions = env.action_space.n
+        self.action_space = [i for i in range(self.n_actions)]
         self.memory_size = max_memory_size
+        
+        self.memory = ReplayMemory(self.observation_shape[0], max_memory_size, batch_size)
         
         self.epsilon = epsilon
         self.eps_decay = eps_decay
@@ -128,132 +124,122 @@ class DQNAgent():
         self.replace_target_count = replace
         self.checkpoint_dir = checkpoint_dir
 
-        self.learn_step_counter = 0
-
-        self.memory_counter = 0 # Counter to keep track of how many memories we have stored
+        self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
 
         # Q Evaluation Network
-        # Hidden neurons defaulted to 128 (for testing)
-        self.Q_eval = DuelingDeepQNetwork(self.learning_rate, input_dims=self.observation_space, fc1_dims=self.hidden_neurons,
-                                    fc2_dims=self.hidden_neurons, n_actions=self.n_actions, name='surround_dueling_ddqn_q_eval',
+        self.network = DuelingDeepQNetwork(self.learning_rate, input_dims=self.observation_shape[0], nn_dims=hidden_neurons,
+                                    n_actions=self.n_actions, name='surround_dueling_ddqn',
                                     checkpoint_dir=self.checkpoint_dir)
         
-        self.Q_next = DuelingDeepQNetwork(self.learning_rate, input_dims=self.observation_space, fc1_dims=self.hidden_neurons,
-                                    fc2_dims=self.hidden_neurons, n_actions=self.n_actions, name='surround_dueling_ddqn_q_eval',
+        self.target_network = DuelingDeepQNetwork(self.learning_rate, input_dims=self.observation_shape[0], nn_dims=hidden_neurons,
+                                    n_actions=self.n_actions, name='surround_dueling_ddqn_target',
                                     checkpoint_dir=self.checkpoint_dir)
-
-        # Experience memory
-        # The video guide I(Axel) followed uses multiple arrays instead of a deque. 1 for each experience: (state, action, reward, new_state, terminal?)
-        self.state_memory = np.zeros((self.memory_size, *observation_space), dtype=np.float32) ### DEBUGGING: Obersvation_space = [2]
-        self.action_memory = np.zeros(self.memory_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.memory_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.memory_size, dtype=bool)
-        self.truncated_memory = np.zeros(self.memory_size, dtype=bool)
-        self.new_state_memory = np.zeros((self.memory_size, *observation_space), dtype=np.float32)
-
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.target_network.eval()
         
-        #Policy Network
-        #self.network = 
-        #self.target_network = 
-        #self.target_network.load_state_dict(self.network.state_dict())
-        #self.target_network.eval()
-        #self.network.eval()
+        self.optimiser = optim.Adam(self.network.parameters())
         
-        #Network Optimiser - Have a look at different optimisers
-        #The optimiser updates the weightings in the nn to predict better q-values.
-        #self.optimiser = torch.optim.Adam(self.network.parameters(), lr=learning_rate)
-    
-    def store_transition(self, state, action, reward, next_state, terminated, truncated):
-        #Store transitions in memory so that we can use them for experience replay
-        index = self.memory_counter % self.memory_size    # Wraps around when memory is full, overwriting oldest memories
-        self.state_memory[index]      = state
-        self.action_memory[index]     = action
-        self.reward_memory[index]     = reward
-        self.new_state_memory[index]  = next_state
-        self.terminal_memory[index]   = terminated
-        self.truncated_memory[index]  = truncated
-
-        self.memory_counter          += 1
+        self.transition = []
+        
+        self.testing = False
          
-    def action(self, observation): # Chooses action
-        if np.random.random(1) > self.epsilon: # Best action (Exploitation)
-            state = T.tensor(np.array(observation), dtype=T.float).to(self.Q_eval.device) # Convert state to tensor,prepare Q_eval for preprocessing
-            _, advantage = self.Q_eval.forward(state) # Push state through network and get advantage function
-            # actions = self.Q_eval.forward(state) # actions is all the Q-value outputs from the network after a forward pass
-            action = T.argmax(advantage).item() # Get the action with the highest advantage
-        else: # Random action (Exploration)
-            action = np.random.choice(self.action_space) # Choose a random action from the action space
-
+    def action(self, state: np.ndarray):
+        '''Action choice based on epsilon-greedy policy, returns the action as a np.ndarray'''
+        if np.random.random() > self.epsilon: 
+            action = self.network(T.FloatTensor(state).to(self.device)).argmax()
+            action = action.detach().cpu().numpy() 
+        else: 
+            action = np.random.choice(self.action_space) 
+        
+        if not self.testing:
+            self.transition = [state, action]
         return action
     
-    def replace_target_network(self):   # Handles replacing the target network
-        if self.learn_step_counter % self.replace_target_count == 0:  # Do so after certain number of steps
-            self.Q_next.load_state_dict(self.Q_eval.state_dict())     # Load the state dictionary from the evaluation q network to the q_next network
-
+    def replace_target_network(self, count):
+        '''Updates target network on regular intervals'''
+        if count % self.replace_target_count == 0:
+            self.target_network.load_state_dict(self.network.state_dict())
+            
     def save_models(self):
-        self.Q_eval.save_checkpoint()
-        self.Q_next.save_checkpoint()
+        self.network.save_checkpoint()
+        self.target_network.save_checkpoint()
 
     def load_models(self):
         self.Q_eval.load_checkpoint()
         self.Q_next.load_checkpoint()
 
     def learn(self):
-        # Learn from batch of experience . Experience Replay!
-        # Start learning as soon as we've filled a batch of memories (to sample from when learning)
-        if self.memory_counter < self.batch_size: # 
-            return
+        batches = self.memory.sample()
+        loss = self.calculate_loss(batches) # make helper function
+        self.optimiser.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(self.network.parameters(), 1) # clips gradients between -1 and 1, can change
+        self.optimiser.step()
+        return loss.item()
+
+    def calculate_loss(self, samples):
+        """Helper function to simplify learn() function, returns the loss"""
+        state = T.FloatTensor(samples["states"]).to(self.device)
+        next_state = T.FloatTensor(samples["next_states"]).to(self.device)
+        action = T.LongTensor(samples["actions"].reshape(-1, 1)).to(self.device)
+        reward = T.FloatTensor(samples["rewards"].reshape(-1, 1)).to(self.device)
+        done = T.FloatTensor(samples["dones"].reshape(-1, 1)).to(self.device)
+
+        current_q = self.network(state).gather(1, action)
+        next_q = self.target_network(next_state).max(dim=1, keepdim=True)[0].detach()
+        if_done = 1 - done # sets gamma*Q to zero if step lead to termination or truncation
+        target = (reward + self.gamma * next_q * if_done).to(self.device)
+
+        loss = F.smooth_l1_loss(current_q, target) # Currently on smooth_l1_loss, can change to MSE or others.
+
+        return loss
+
+    def decay_epsilon(self, linear=False):
+        if linear:
+            self.epsilon = max(self.eps_min, self.epsilon - self.eps_decay)
+        else:
+            self.epsilon = max(self.eps_min, self.epsilon * self.eps_decay)
         
-        self.Q_eval.optimizer.zero_grad()      # Set up optimizer (Zero the gradients of it)
-
-        self.replace_target_network() # Want to replace our target network 
-
-        max_mem = min(self.memory_counter, self.memory_size)                # Max memory we can use
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)       # Randomly take *batch_size* numbers from range(max_mem)
-        # Batch is an array of indices we will use to sample from memory
-        batch_index = np.arange(self.batch_size, dtype=np.int32)               # Create an array of numbers from 0 to batch_size
-
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)          # Convert all the bacthes to tensors
-        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)  
-        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)        
-        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)   
-
-        action_batch = self.action_memory[batch]                                         # Get the actions from the batch
-
-        V_s, A_s = self.Q_eval.forward(state_batch)
-        V_s_, A_s_ = self.Q_next.forward(new_state_batch)
-
-        V_s_eval, A_s_eval = self.Q_eval.forward(new_state_batch)
-
-        q_pred = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True)))[batch_index, action_batch]
-        q_next = T.add(V_s_, (A_s_ - A_s_.mean(dim=1, keepdim=True)))
-        q_eval = T.add(V_s_eval, (A_s_eval - A_s_eval.mean(dim=1, keepdim=True)))
-
-        max_actions = T.argmax(q_eval, dim=1)
-
-        q_next[terminal_batch] = 0.0
-        q_target = reward_batch + self.gamma*q_next[batch_index, max_actions]
-
-
-        #q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch] # Shape [64]  # Get the Q-values of the actions we took
-        #q_next = self.Q_eval.forward(new_state_batch) # SHape: [64, 3]                   # Pass a bactch of states into the network to get Q-values for each action in each state
-        #q_next[terminal_batch] = 0.0                                                     #  Set Q-values of all terminal states to 0
-
-        #q_target = reward_batch + self.gamma*T.max(q_next, dim=1)[0]                     # Calculate the target Q-values using the Bellman equation
+    def train(self, episodes):
+        self.testing = False
+        tracked_info = {
+            "scores":[],
+            "losses":[],
+            "epsilons":[]
+        }
+        learn_count = 0
         
-        loss = self.Q_eval.loss(q_target, q_pred).to(self.Q_eval.device)                    # Mean-squared error between q_next and q_eval
-        loss.backward()       # Backpropagate the loss. Tells us how much the weights of the nn affect the loss. Helps us adjust the weights to minimize loss
-        self.Q_eval.optimizer.step()   # Step the optimizer. Adjust the weights of the nn based on the gradients calculated in the loss.backward() step
-        self.learn_step_counter += 1
-
-        # Decrement epsilon (explore rate)
-        if self.learn_step_counter % 1000 == 0:
-            self.epsilon = max(self.epsilon * self.eps_decay, self.eps_min) 
-
-        
-        
-    # Axel: I havent added a target nn yet
-    def update_target(self):
-        self.target_network.load_state_dict(self.network.state_dict())
-        
+        state, _ = self.env.reset()
+        for episode in tqdm(range(1,episodes+1)):
+            score = 0
+            while True:
+                action = self.action(state)
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                state = next_state
+                score += reward
+                done = terminated or truncated
+                if not self.testing:
+                    self.transition += [reward, next_state, done]
+                    self.memory.store_transition(*self.transition)
+                
+                if done:
+                    state, _ = self.env.reset()
+                    tracked_info["scores"].append(score)
+                    score = 0
+                    if episode > 10 and episode % 10 == 0:
+                        self.save_models()
+                    break
+                
+                if len(self.memory) >= self.batch_size:
+                    loss = self.learn()
+                    tracked_info["losses"].append(loss)
+                    learn_count += 1
+                    self.decay_epsilon(linear=False)
+                    tracked_info["epsilons"].append(self.epsilon)
+                    self.replace_target_network(learn_count)
+                
+        self.env.close()
+        return tracked_info
+                
+            
 
--- main.py
+++ main.py
@@ -9,48 +9,15 @@ from termcolor import colored
 import torch
 
 
-env = gym.make("ALE/Surround-v5", obs_type='ram') # "ALE/Surround-v5"  #render_mode'human' ?
+env = gym.make("CartPole-v1", render_mode='rgb_array') # "ALE/Surround-v5"  #render_mode'human' ?
 
-agent = DQNAgent(learning_rate=5e-4, batch_size=64, observation_space=env.observation_space.shape, \
-                  n_actions=env.action_space.n, epsilon=1.0, eps_decay=0.99, eps_min=0.1, gamma=0.95, replace = 50)
-scores, eps_history = [], []
+agent = DQNAgent(env, 0.003, 64, 0.95, 1)
 total_episodes = 250
 load_checkpoint = False
-data_store = Grapher()
-
 
 if load_checkpoint:
     agent.load_models()
 
-for i in tqdm(range(total_episodes)): # Loop through the episodes
-    score = 0
-    terminated = False
-    truncated = False   # Truncated is for time limits when time is not part of the observation space / state
-    (state,_) = env.reset()
-    counter = 0
-    while not terminated and not truncated:
-        #env.render()
-        #if counter % 100 == 0: print(colored(f"State Number: {counter}, Epsilon: {agent.epsilon}", "red"))
-        counter += 1
-        action = agent.action(state)                                        # Agent picks an action
-        next_state, reward, terminated, truncated, info = env.step(action)  # Env returns next state, reward, and if it's done
-        score += reward                                                     # Total score is updated
-        agent.store_transition(state, action, reward, next_state, terminated, truncated) # Store the experience
-        agent.learn()                                                       # Learns from the experience
-        state = next_state                                                  # Update the state                                            
-        if terminated or truncated: print(colored("Episode terminated or truncated", "red"))
-    scores.append(score)                                     # Episode done. Append the score to the scores list
-    eps_history.append(agent.epsilon)                      # Append the epsilon value to the eps_history list, for data analysis
+data = agent.train(total_episodes)
 
-    avg_score = np.mean(scores[-100:])                  # Average score over last 100 episodes
-
-    print('episode ', i, 'score %.2f' % score,
-            '  average score %.2f' % avg_score,
-            ' epsilon %.2f' % agent.epsilon)
-    
-if i > 10 and i % 10 == 0:
-    agent.save_models()
-
-x = [i+1 for i in range(total_episodes)]
-filename = 'surround.png'
-plotLearning(x, scores, eps_history, filename)
+grapher = Grapher(data)
