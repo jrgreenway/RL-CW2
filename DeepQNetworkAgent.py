165c8a9b65d625cb27b27876a2aa38ce318ecfef
AxelGitlafsson <146286107+AxelGitlafsson@users.noreply.github.com>
2024-05-04 19:12:43 +0200

    Refactored nStep  :)

--- DeepQNetworkAgent.py
+++ DeepQNetworkAgent.py
@@ -13,16 +13,14 @@ import os
 import math
 from segment_tree import SumSegmentTree, MinSegmentTree
 from collections import deque
-from typing import Deque, Dict, List, Tuple
 
 from tqdm import tqdm                           # For file joining operations to handle model checkpointing
 
 
 class nStepReplayMemory:
     """Converts to n-step transitions and stores them in a cyclic buffer."""
-
-    #
-    def __init__(self, observation_dimensions: int, memory_size: int, batch_size: int = 32, n_step: int = 3, gamma: float = 0.99):
+    # observation_dimensions: int, memory_size: int, batch_size: int = 32, n_step: int = 3, gamma: float = 0.99)
+    def __init__(self, observation_dimensions, memory_size, batch_size=32, n_step=3, gamma=0.99):
 
         self.state_memory = np.zeros([memory_size, observation_dimensions], dtype=np.float32)
         self.next_state_memory = np.zeros([memory_size, observation_dimensions], dtype=np.float32)
@@ -34,7 +32,7 @@ class nStepReplayMemory:
         self.memory_index = 0    # pointer to the current location in the buffer
         self.size = 0   # current size of the buffer
         
-        # for N-step Learning
+        # for N-step
         self.n_step_buffer = deque(maxlen=n_step)   
         self.n_step = n_step
         self.gamma = gamma
@@ -50,7 +48,6 @@ class nStepReplayMemory:
         self.memory_index = (self.memory_index + 1) % self.max_memory_size
         self.size = min(self.size + 1, self.max_memory_size)
 
-
     # store the transition in the buffer and return the n-step transition if ready
     # input experience - output n-step transition
     # state: np.ndarray, action: np.ndarray, reward: float, next_state: np.ndarray, done: bool) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray, bool]:
@@ -59,14 +56,22 @@ class nStepReplayMemory:
         experience = (state, action, reward, next_state, done)  # single step transition
         self.n_step_buffer.append(experience)
 
-        # single step transition is not ready
         if len(self.n_step_buffer) < self.n_step:   # if n_step_buffer is not full do not return anything
             return ()
         
-        # make a n-step transition.     Look into the future
-        reward, next_state, done = self._get_n_step_info(self.n_step_buffer, self.gamma) # Look into the future, sets n-step values
+        # Calculate n-step return
+        last_experience = self.n_step_buffer[-1]           # latest transition in buffer
+        _, _, reward_nstep, next_state_nstep, done_nstep = last_experience
+
+        for experience in reversed(list(self.n_step_buffer)[:-1]): # Iterate backwards, excluding the last experience
+            r, n_s, d = experience[-3:]
+            reward_nstep = r + self.gamma * reward_nstep * (1 - d)
+            next_state_nstep, done_nstep = (n_s, d) if d else (next_state_nstep, done_nstep) # if any of the transitions is done, the n-step return is done    
+        
+        reward, next_state, done = reward_nstep, n_s, d      # n-step return  
         state, action = self.n_step_buffer[0][:2]  # state and action become the first state and action in the n-step buffer
         
+        # store the n-step transition in the memory
         self.state_memory[self.memory_index] = state
         self.next_state_memory[self.memory_index] = next_state
         self.action_memory[self.memory_index] = action
@@ -77,43 +82,7 @@ class nStepReplayMemory:
         
         return self.n_step_buffer[0] # return the single step transition
 
-    def sample_batch(self) -> Dict[str, np.ndarray]:
-        indices = np.random.choice(self.size, size=self.batch_size, replace=False)
-
-        return dict(
-            obs=self.state_memory[indices],
-            next_state=self.next_state_memory[indices],
-            acts=self.action_memory[indices],
-            rews=self.reward_memory[indices],
-            done=self.done_memory[indices],
-            # for N-step Learning
-            indices=indices,
-        )
-    
-    def sample_batch_from_idxs(self, indices: np.ndarray) -> Dict[str, np.ndarray]:
-        # for N-step Learning
-        return dict(
-            obs=self.state_memory[indices],
-            next_state=self.next_state_memory[indices],
-            acts=self.action_memory[indices],
-            rews=self.reward_memory[indices],
-            done=self.done_memory[indices],
-        )
-    
-    def _get_n_step_info(self, n_step_buffer: Deque, gamma: float) -> Tuple[np.int64, np.ndarray, bool]:
-        """Return n step reward, next_state, and done."""
-        # info of the last transition
-        r, next_state, done = n_step_buffer[-1][-3:]  # n_step_buffer[-1][-3:] means 
-
-        for transition in reversed(list(n_step_buffer)[:-1]):
-            r, n_s, d = transition[-3:]
-
-            r = r + gamma * r * (1 - d)
-            next_state, done = (n_s, d) if d else (next_state, done)
-
-        return r, next_state, done
-
-    def __len__(self) -> int:
+    def __len__(self):
         return self.size
 
 # THIS CHANGES WITH N STEP
