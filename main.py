1f45420c4b24d01df85c0251728582eda8f8ed47
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-05-07 23:56:43 +0100

    undo continous mods

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from grapher import Grapher
 import logging
 
 env = gym.make("LunarLander-v2", continuous=False) # "ALE/Surround-v5"  #render_mode'human' ?
+#CHANGE BACK TO DISCRETE
 
 agent = DQNAgent(env, 0.0001, 64, 0.99, 0.3, 0.01, max_memory_size=10000)
 total_frames = int(1e5) # Think we should consider changing to number of frames not episodes
