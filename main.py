1581bdd0a4ece1de798191010b8c14cc79a1afc7
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-05-08 22:13:28 +0100

    chore: Refactor main.py for LunarLander-v2 environment and video recording

--- main.py
+++ main.py
@@ -2,18 +2,37 @@ import gymnasium as gym
 from DeepQNetworkAgent import DQNAgent
 from grapher import Grapher
 import logging
+from gymnasium.wrappers import RecordVideo
 
-env = gym.make("LunarLander-v2", continuous=False) 
+total_frames = int(1e5)
 
+def step_trigger(step):
+    return step == 0 or (step > total_frames-1000)
 
-agent = DQNAgent(env, 0.001, 64, 0.99, 0.3, 0.01, max_memory_size=10000)
-total_frames = int(1e5) 
-load_checkpoint = False
+def ep_trig(episode):
+    return episode == 0 or (episode % 5 == 0 and episode > 250)
 
-if load_checkpoint:
-    agent.load_models()
+for iter in range(5):
+    env = gym.make("LunarLander-v2", continuous=False, render_mode='rgb_array')
+    env = RecordVideo(env, video_folder="videos/", episode_trigger=ep_trig, video_length=0)
 
-data, params = agent.train(total_frames)
+    agent = DQNAgent(env,
+                    0.001,
+                    64,
+                    0.99,
+                    0.3,
+                    0.01,
+                    max_memory_size=10000,
+                    hidden_neurons=64,
+                    log=False)
 
-grapher = Grapher(data, params)
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+
+    data, params = agent.train(total_frames)
+
+    grapher = Grapher(data, params)
+
 logging.shutdown()
