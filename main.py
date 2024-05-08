9cd8fe18a73ea5a0d8cfe91d685c2f51048f2dc0
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-05-08 22:08:44 +0100

    refactor: Update DQNAgent hyperparameters and remove unnecessary code

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from DeepQNetworkAgent import DQNAgent
 from grapher import Grapher
 import logging
 
-env = gym.make("LunarLander-v2", continuous=False) # "ALE/Surround-v5"  #render_mode'human' ?
-#CHANGE BACK TO DISCRETEs
+env = gym.make("LunarLander-v2", continuous=False) 
 
-agent = DQNAgent(env, 0.0001, 64, 0.99, 0.3, 0.01, max_memory_size=10000)
-total_frames = int(1e5) # Think we should consider changing to number of frames not episodes
+
+agent = DQNAgent(env, 0.001, 64, 0.99, 0.3, 0.01, max_memory_size=10000)
+total_frames = int(1e5) 
 load_checkpoint = False
 
 if load_checkpoint:
